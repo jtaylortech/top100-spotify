96676c5a796ac31fa65b31c28bbf36561a98de66
Jarred Taylor <jarrede20@gmail.com>
2022-08-16 19:50:27 -0400

    search spotify for the songs passed from the inputed date and correlating hot 100 songs AND adding them to a playlist

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from spotipy.oauth2 import SpotifyOAuth
 JT_SPOTIFY_ID = jt_spotify_ID
 JT_SPOTIFY_SECRET = jt_spotify_secret
 
+# Scrapes the Billbaord 100
 users_date = input('Which year do you want to travel to? \n Please type the date in the "YYYY-MM-DD" format: ')
 
 response = requests.get("https://www.billboard.com/charts/hot-100/" + users_date)
@@ -16,6 +17,7 @@ soup = BeautifulSoup(response.text, 'html.parser')
 song_names_spans = soup.find_all("span", class_="chart-element__information__song")
 song_names = [song.getText() for song in song_names_spans]
 
+# Spotify Authentication
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
@@ -28,3 +30,24 @@ sp = spotipy.Spotify(
 )
 
 user_id = sp.current_user()["id"]
+print(user_id)
+
+# Searching Spotify for songs by title
+song_uris = []
+year = users_date.split("-")[0]
+
+for song in song_names:
+    result = sp.search(q=f"track:{song}year:{year}",type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} does not exist in Spotify. Skipped.")
+
+# Creating a new private playlist for Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{users_date} Billboard 100", public=False)
+print(playlist)
+
+# Adding songs from found into the new playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
